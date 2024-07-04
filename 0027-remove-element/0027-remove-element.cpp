6df812ca760a01061c20cd7eddb81d254a22e61e
Suraj Singh Bhandari <148539006+Bhandariji-Suraj@users.noreply.github.com>
2024-07-04 12:24:01 +0530

    Time: 5 ms (27.56%), Space: 10.5 MB (7.84%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -2,10 +2,12 @@ class Solution {
 
 public:
     int removeElement(vector<int>& nums, int val) {
+        //M-1 Using remove and erase operations
         if(nums.empty()) return 0;
         nums.erase(remove(nums.begin(),nums.end(),val),nums.end());
         int count = nums.size();
         
+        //M-2 using simple approach of duplicate array
         // if(nums.empty()) return 0;
         // vector<int> ans;
         // int count=0;
