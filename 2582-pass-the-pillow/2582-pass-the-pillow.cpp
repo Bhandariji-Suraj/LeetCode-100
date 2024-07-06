66aaa605db64e59e67e8644ac9e11d457bc1c0a3
Suraj Singh Bhandari <148539006+Bhandariji-Suraj@users.noreply.github.com>
2024-07-06 14:03:25 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (30.58%) - LeetHub

--- 2582-pass-the-pillow/2582-pass-the-pillow.cpp
+++ 2582-pass-the-pillow/2582-pass-the-pillow.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int passThePillow(int n, int time) {
-        int a = time/(n-1);
-        int b = time%(n-1);
         int res = 0;
-        if(a%2==0)
+        if((time/(n-1))%2==0)
         {
-            res = 1+b;
+            res = (1+time%(n-1));
         }
         else
         {
-            res = n-b;
+            res = (n-time%(n-1));
         }
         return res;
         
