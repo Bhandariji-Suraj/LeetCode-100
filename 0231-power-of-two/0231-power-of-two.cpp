36bbb6eca34ad130191cf9605927797d4d9193cc
Suraj Singh Bhandari <148539006+Bhandariji-Suraj@users.noreply.github.com>
2024-07-27 15:00:48 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (18.65%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -2,7 +2,14 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) 
     {
-        if (n <= 0) return false; // Exclude negative numbers and zero
-        return (n & (n - 1)) == 0;    }
+        if(n<1) return false;
+        while(n%2==0)
+        {
+            n=n/2;
+        }
+        return n==1;
+        // if (n <= 0) return false; // Exclude negative numbers and zero
+        // return (n & (n - 1)) == 0;    
+    }
 
 };
