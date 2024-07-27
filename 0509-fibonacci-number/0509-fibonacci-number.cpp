45829b1e6b449727047ebbc73863d98701adbf8d
Suraj Singh Bhandari <148539006+Bhandariji-Suraj@users.noreply.github.com>
2024-07-27 10:39:27 +0530

    Time: 15 ms (15.53%), Space: 7.4 MB (22.13%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -4,6 +4,7 @@ public:
     {
         if(n==0) return 0;
         if(n==1) return 1;
-        return fib(n-1) + fib(n-2);   
+        int ans = fib(n-1) + fib(n-2);   
+        return ans;
     }
 };
